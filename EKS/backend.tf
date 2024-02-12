97df413a1c79b29bf98fb7b795e0ca7eaab187d0
veeru072 <147216521+veeru072@users.noreply.github.com>
2024-02-12 18:18:00 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ci-cd-base-bucket-1"  # Replace with your actual S3 bucket name
+    bucket = "myp1-bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-south-1" 
   }
